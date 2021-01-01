6fb8e850db52054ad237e4e002e52c992268793e
lynn7mitchell <lynn7mitchell@gmail.com>
2020-12-31 18:47:53 -0600

    notes for nested components, props, and conditional rendering using data binding

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>{{ words }}</h1>
+  <!-- header is a prop just like in react -->
+  <!-- you can use the colon to data bind to whatever you want such as the data object -->
+  <Modal :header='header' :text="text" theme='sale'/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+
+  <!-- ref is similar to a query selector document.getElementById -->
+  <!-- <input type="text" ref="name" />
+  <button @click="handleClick">click me</button> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from './components/Modal'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
+  // name is not required
   name: "App",
+  // any outside components that are imprted need to be mentioned here
   components: {
-    HelloWorld
+    // HelloWorld,
+    Modal
+  },
+  data() {
+    // must return an object
+    return {
+      words: "Words from the data function!",
+      header: 'Message from app component',
+      text: 'Some text from the app component'
+    };
+  },
+  // this = the current component
+  methods: {
+    handleClick() {
+      // $ref = getElementByRefName
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 };
 </script>
 
 <style>
+/* styles are global (not just for this component) */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +52,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="backdrop">
+        <!-- checks theme prop. Since it is sale then add the class sale -->
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+// how to accepts props from parent components
+export default{
+    props:['header', 'text', 'theme']
+}
+</script>
+
+<style scoped>
+/* styles will only be applied to this component because of the scoped attribute in the style tag */
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #fff;
+    border-radius: 10px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+h1{
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+}
+.modal.sale{
+    background: crimson;
+    color: #fff
+}
+.modal.sale h1{
+    color: #fff
+}
+</style>
