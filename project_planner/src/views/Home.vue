1cccec82bcc36cbc2a2ce4574997b3ec3c7a8d80
lynn7mitchell <lynn7mitchell@gmail.com>
2021-01-06 20:42:56 -0600

    added project_planner project, deleted anything not needed, set up data for project

---
+++ project_planner/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <!-- if the projects array has anything in it -->
+   <div v-if="projects.length">
+     <div v-for="projects in projects" :key="projects.id">
+       <p>{{projects.title}}</p>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+ components: {},
+ data(){
+   return{
+     projects: []
+   }
+ },
+//  componentDidMount()
+  mounted(){
+   fetch('http://localhost:3000/projects')
+   .then(res => res.json())
+   .then(data => this.projects = data)
+   .catch(err => console.error(err.message))
+ }
+}
+</script>
